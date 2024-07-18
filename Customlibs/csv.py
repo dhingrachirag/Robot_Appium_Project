6612166cb458fd8c2e59cd616b7a2bf1a293ad7d
Chirag Dhingra <46193115+dhingrachirag@users.noreply.github.com>
2024-07-18 13:31:01 +0530

    Add files via upload

--- Customlibs/csv.py
+++ Customlibs/csv.py
@@ -1,1 +1,10 @@
+import csv
 
+
+def read_csv(filename):
+    data = []
+    with open(filename, 'rb') as csvfile:
+        reader1 = csv.reader1(csvfile)  # type: object 
+        for row in reader1:
+            data.append(row)
+    return data
